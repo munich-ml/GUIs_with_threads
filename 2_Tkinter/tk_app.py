0d5e0be7eb344230cf0408f8e9dea748632b6d96
Holger <holger.steffens@gmail.com>
2022-05-03 10:51:06 +0200

    object orientation

--- 2_Tkinter/tk_app.py
+++ 2_Tkinter/tk_app.py
@@ -1,34 +1,42 @@
 import tkinter as tk
-from tkinter import tix
-import sys
+from collections import OrderedDict
 
 PADX, PADY = 5, 3      # Widget padding
 
-window = tk.Tk()
-upper_frame = tk.Frame()
-lower_frame = tk.Frame()
-for frame in (upper_frame, lower_frame):
-    frame.pack(padx=PADX, pady=PADY, fill=tk.X)
 
-labels = ("input", "fibunacci(input)", "compute time [s]")
-for row, text in enumerate(labels):
-    label = tk.Label(upper_frame, text=text)
-    label.grid(row=row, column=0, padx=PADX, pady=PADY, sticky="w")
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Tkinter threading demonstrator')
+        
+        self.frames = OrderedDict()
+        for name in ("upper", "lower"):
+            self.frames[name] = tk.Frame()
+            self.frames[name].pack(padx=PADX, pady=PADY, fill=tk.X)
 
-input_ = tk.Spinbox(upper_frame, width=10, from_= 10, to=50)
-entry_fib = tk.Entry(upper_frame)
-entry_time = tk.Entry(upper_frame)
-for row, widget in enumerate((input_, entry_fib, entry_time)):
-    widget.grid(row=row, column=1, padx=PADX, pady=PADY, sticky="w")
+        self.labels = OrderedDict()
+        for row, name in enumerate(("input", "fibunacci(input)", "compute time [s]")):
+            self.labels[name] = tk.Label(self.frames["upper"], text=name)
+            self.labels[name].grid(row=row, column=0, padx=PADX, pady=PADY, sticky="w")
+
+        self.widgets = OrderedDict()
+        self.widgets["input"] = tk.Spinbox(self.frames["upper"], width=10, from_= 10, to=50)
+        self.widgets["fibunacci"] = tk.Entry(self.frames["upper"])
+        self.widgets["time"] = tk.Entry(self.frames["upper"])
+        for row, widget in enumerate(self.widgets.values()):
+            widget.grid(row=row, column=1, padx=PADX, pady=PADY, sticky="w")
+
+        self.buttons = OrderedDict()
+        self.buttons["main"] = tk.Button(self.frames["lower"], text="compute in main thread", command=self.on_button_main)
+        self.buttons["worker"] = tk.Button(self.frames["lower"], text="compute in worker thread", command=self.on_button_main)
+        for column, button in enumerate(self.buttons.values()):
+            button.grid(row=0, column=column, padx=PADX, pady=PADY)
+            
+            
+    def on_button_main(self):
+        print("button main thread clicked")
 
 
-def on_button_main():
-    print("button main thread clicked")
-    
-button_main = tk.Button(lower_frame, text="compute in main thread", command=on_button_main)
-button_worker = tk.Button(lower_frame, text="compute in worker thread", command=on_button_main)
-for column, button in enumerate((button_main, button_worker)):
-    button.grid(row=0, column=column, padx=PADX, pady=PADY)
-
-
-window.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
